73fd5034ce4aa1d1882b081851f80a99f134fe58
fahim0009 <fahim.amin71@gmail.com>
2022-06-01 12:48:26 +0600

    category hide

--- resources/views/frontend/inc/header.blade.php
+++ resources/views/frontend/inc/header.blade.php
@@ -12,7 +12,7 @@
             <span class="iconify text-danger" data-icon="charm:menu-hamburger"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNavDropdown">
-                
+
             <ul class="navbar-nav ms-auto navCustomRight">
                 <li class="nav-item simple-link m-1">
                     <a href="{{ route('homepage')}}" class="nav-link rounded">Home</a>
@@ -23,16 +23,16 @@
                 <li class="nav-item simple-link m-1">
                     <a href="{{ route('commercial')}}" class="nav-link rounded">Commercial</a>
                 </li>
-                <li class="nav-item simple-link m-1">
+                {{-- <li class="nav-item simple-link m-1">
                     <a href="{{ route('category')}}" class="nav-link rounded">Category</a>
-                </li>
-                
+                </li> --}}
+
                 <li class="nav-item m-1 ">
                     <a class="nav-link rounded "
                         style="background-color: #00404D; font-family: 'Sinkin Sans-light-300'; font-weight: normal;"
                         href="{{ route('contact')}}">Contact us</a>
                 </li>
-               
+
             </ul>
         </div>
     </div>
